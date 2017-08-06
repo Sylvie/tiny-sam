a0221e9e849b9b01c34767c7f8fe2fc335c9a3cb
Sylvie <Sylvie@users.noreply.github.com>
2017-08-06 19:56:37 +0200

    SMB-16: Toilettage des fichiers Makefile.am et manual/Makefile.mk
    Points principaux:
    - Effaçage des commandes inutiles
    - Adaptation de la construction pour passer la vérification distcheck
    - Déplacement de certaines parties de la cible repo-clean vers distclean
    - Utilisation de pour localiser les fichiers sources $(top_srcdir) dans manual/Makefile.mk

--- manual/Makefile.mk
+++ manual/Makefile.mk
@@ -1,45 +1,42 @@
 manual_filename = manual.tex
 manual_directory := "manual/"
-manual_sources := $(srcdir)/manual/$(manual_filename) $(srcdir)/manual/bibliography.bib
+manual_sources := $(top_srcdir)/manual/$(manual_filename) $(top_srcdir)/manual/bibliography.bib
 manual_pdf := $(builddir)/manual/manual.pdf
 
-EXTRA_DIST += $(manual_sources)
+EXTRA_DIST += $(manual_sources) $(top_srcdir)/manual/latexmkrc
 
 if HAVE_LATEXMK
 EXTRA_DIST += $(manual_pdf)
 endif
 
-LATEXMK = latexmk -r $(srcdir)/manual/latexmkrc -output-directory=$(manual_directory)
-LATEXMK_CLEAN = latexmk -r ../$(srcdir)/manual/latexmkrc
+LATEXMK_BUILD = $(LATEXMK) -r $(top_srcdir)/manual/latexmkrc -output-directory=$(manual_directory)
+LATEXMK_CLEAN = $(LATEXMK) -r ../$(top_srcdir)/manual/latexmkrc
 
 prepare_manual_directory:
 	if [ "$(top_srcdir)" != "$(top_builddir)" ]; then \
 		mkdir -p $(manual_directory) && \
-		cp $(srcdir)/manual/bibliography.bib $(manual_directory) && \
-		cp $(srcdir)/manual/manual.tex $(manual_directory); \
+		cp $(top_srcdir)/manual/bibliography.bib $(manual_directory) && \
+		cp $(top_srcdir)/manual/manual.tex $(manual_directory); \
 	fi
 
 pdf-local-manual : $(manual_pdf)
 
 %.pdf : %.tex | prepare_manual_directory
-	$(LATEXMK) -pdf $^
-
-dist-local-manual: $(outputFile)
-	cp $(manualTargets) ../$(distDir)
+	$(LATEXMK_BUILD) -pdf $^
 
 clean-local-manual:
 	if [ -d "$(manual_directory)" ] && [ -e "$(manual_directory)$(manual_filename)" ] ; then \
 		cd $(manual_directory) && \
+		pwd && \
 		$(LATEXMK_CLEAN) -c $(manual_filename) ; \
 	fi
 
-
-repo-clean-local-manual:
-	if [ -d "$(manual_directory)" ] && [ -e "$(manual_directory)$(manual_filename)" ]; then \
-		cd $(manual_directory) && \
-		$(LATEXMK_CLEAN) -C $(manual_filename) ; \
-	fi
+distclean-local-manual:
 	if [ -d "$(manual_directory)" ] && [ "$(top_srcdir)" != "$(top_builddir)" ]; then \
 		rm -f manual/bibliography.bib ; \
 		rm -f manual/manual.tex ; \
+	fi
+	if [ -d "$(manual_directory)" ] && [ -e "$(manual_directory)$(manual_filename)" ]; then \
+		cd $(manual_directory) && \
+		$(LATEXMK_CLEAN) -C $(manual_filename) ; \
 	fi
