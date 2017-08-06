e24682a7ae051d2c5cbf03652a344d6701fa4de1
Sylvie <Sylvie@users.noreply.github.com>
2017-08-06 17:21:53 +0200

    SMB-16: Gestion des cibles "*clean" pour le manuel
    Durant le nettoyage, le Makefile(.am) principal appelle des cibles
    spécifiques pour nettoyer les fichiers compilés pour le manuel.
    
    Point épineux:
    - Apparemment, latexmk ne permet pas de nettoyer un sous-dossier.
      Par conséquent, les commandes sont exécutées directement dans le
      dossier $(builddir)/manual/.
    - Latexmk ne permet pas non plus de nettoyer les fichiers compilés pour
      un fichier source qui n'est pas dans le dossier courant. Du coup, il
      faut aussi copier le fichier source manual.tex dans le dossier de
      construction du manuel.
    
    Principaux changements:
    - Ajout d'une variable LATEXMK_CLEAN définissant la commande latexmk
      avec les options nécessaires pour le nettoyage.
    - Le nettoyage de type "clean" appelle latexmk -c pour effacer les
      fichiers intermédiaires.
    - Le nettoyage de type "repo-clean" appelle latexmk -C pour effacer en
      plus le fichier pdf. Cette commande efface aussi les fichiers qui ont
      été copiés. Attention: Il faut vérifier que le dossier de construction
      n'est pas le même que le dossier source, sinon on risque d'effacer les
      originaux!

--- manual/Makefile.mk
+++ manual/Makefile.mk
@@ -5,11 +5,14 @@ manual_pdf := $(builddir)/manual/manual.pdf
 EXTRA_DIST += $(manual_sources) $(manual_pdf)
 
 LATEXMK = latexmk -r $(srcdir)/manual/latexmkrc -output-directory=$(manual_directory)
+LATEXMK_CLEAN = latexmk -r ../$(srcdir)/manual/latexmkrc
+
 
 prepare_manual_directory:
 	if [ "$(srcdir)" != "." ]; then \
-	  mkdir -p $(manual_directory); \
-	  cp $(srcdir)/manual/bibliography.bib $(manual_directory); \
+		mkdir -p $(manual_directory); \
+		cp $(srcdir)/manual/bibliography.bib $(manual_directory); \
+		cp $(srcdir)/manual/manual.tex $(manual_directory); \
 	fi
 
 pdf-local-manual : $(manual_pdf)
@@ -21,4 +24,13 @@ dist-local-manual: $(outputFile)
 	cp $(manualTargets) ../$(distDir)
 
 clean-local-manual:
-	$(LATEXMK) -C
+	cd $(manual_directory); \
+	$(LATEXMK_CLEAN) -c
+
+repo-clean-local-manual:
+	cd $(manual_directory); \
+	$(LATEXMK_CLEAN) -C
+	if [ "$(srcdir)" != "." ]; then \
+		rm manual/bibliography.bib ; \
+		rm manual/manual.tex ; \
+	fi
