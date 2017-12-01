123cbe725dea10458e2e0447910f0c9bc612ac2a
Sylvie <Sylvie@users.noreply.github.com>
2017-12-01 21:06:48 +0100

    SMB-4: Ajout des tests dans Travis et Appveyor (5)
    Correction du fichier test/Makefile.mk.

--- test/Makefile.mk
+++ test/Makefile.mk
@@ -1,6 +1,6 @@
 noinst_PROGRAMS += test/factorial test/GreetingsProviderTests test/total
 TESTS += test/factorial test/GreetingsProviderTests test/total
 
-test_factorial_SOURCES = test/factorial.cpp test/main-catch.cpp test/catch.hpp test/catch_reporter_automake.hpp
-test_GreetingsProviderTests_SOURCES = test/GreetingsProviderTests.cpp src/GreetingsProvider.cpp src/GreetingsProvider.hpp test/main-catch.cpp test/catch.hpp test/catch_reporter_automake.hpp
-test_total_SOURCES = test/factorial.cpp test/GreetingsProviderTests.cpp src/GreetingsProvider.cpp src/GreetingsProvider.hpp test/main-catch.cpp test/catch.hpp test/catch_reporter_automake.hpp
+test_factorial_SOURCES = test/factorial.cpp test/main-catch.cpp test/catch.hpp
+test_GreetingsProviderTests_SOURCES = test/GreetingsProviderTests.cpp src/GreetingsProvider.cpp src/GreetingsProvider.hpp test/main-catch.cpp test/catch.hpp
+test_total_SOURCES = test/factorial.cpp test/GreetingsProviderTests.cpp src/GreetingsProvider.cpp src/GreetingsProvider.hpp test/main-catch.cpp test/catch.hpp
