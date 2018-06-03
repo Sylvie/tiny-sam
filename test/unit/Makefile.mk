3bd6d8011dda57a23fb324cf96e3ac609751b218
Sylvie <Sylvie@users.noreply.github.com>
2018-06-03 09:21:52 +0200

    SMB-30: Déplacement des tests unitaires
    Les tests unitaires existants ont été déplacés dans un sous-dossier
    "test/unit/".
    Deux cibles ont été renommées: factorial est devenue FactorialTests et
    total est devenue UnitTests.

---
+++ test/unit/Makefile.mk
@@ -0,0 +1,11 @@
+noinst_PROGRAMS += test/unit/FactorialTests test/unit/GreetingsProviderTests test/unit/UnitTests
+TESTS += test/unit/FactorialTests test/unit/GreetingsProviderTests test/unit/UnitTests
+
+test_unit_FactorialTests_SOURCES = test/unit/factorial.cpp ${catch_source_files}
+test_unit_FactorialTests_CPPFLAGS = -I $(top_srcdir)/src -I $(top_srcdir)/test
+
+test_unit_GreetingsProviderTests_SOURCES = test/unit/GreetingsProviderTests.cpp src/GreetingsProvider.cpp src/GreetingsProvider.hpp ${catch_source_files}
+test_unit_GreetingsProviderTests_CPPFLAGS = -I $(top_srcdir)/src -I $(top_srcdir)/test
+
+test_unit_UnitTests_SOURCES = test/unit/factorial.cpp test/unit/GreetingsProviderTests.cpp src/GreetingsProvider.cpp src/GreetingsProvider.hpp ${catch_source_files}
+test_unit_UnitTests_CPPFLAGS = -I $(top_srcdir)/src -I $(top_srcdir)/test
