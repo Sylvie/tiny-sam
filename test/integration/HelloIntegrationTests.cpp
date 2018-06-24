ddb0eb57c6e3c85536d0a8811280ffe19aa778b2
Sylvie <sylvie@users.noreply.github.com>
2018-06-24 15:16:19 +0200

    SMB-30: Tentative de tests d'intégration avec AppVeyor (6)
    Le test est capable de lancer le programme (situé dans "./src/") et de lire le résultat (situé dans ".").
    Le dossier courant est "build" (autotools) ou "cmake-build-debug" (CLion).

--- test/integration/HelloIntegrationTests.cpp
+++ test/integration/HelloIntegrationTests.cpp
@@ -35,21 +35,21 @@ void runCommand(const std::string& path, const std::string& program, const std::
     // Must set size of structure
     si.cb = sizeof( si );
 
-    BOOL bRet = CreateProcess( programWithPath.c_str(), // Path to executable file
+    BOOL bRet = CreateProcess( /*programWithPath.c_str()*/ NULL, // Path to executable file
                                commandCString,   // Command string - not needed here
                                NULL,   // Process handle not inherited
                                NULL,   // Thread handle not inherited
                                FALSE,  // No inheritance of handles
                                0,      // No special flags
                                NULL,   // Same environment block as this prog
-                               path.c_str(),   // Current directory - no separate path
+                               /*path.c_str()*/ NULL,   // Current directory - no separate path
                                &si,    // Pointer to STARTUPINFO
                                &pif ); // Pointer to PROCESS_INFORMATION
 
     if( FALSE == bRet )
     {
-        MessageBox( HWND_DESKTOP, "Unable to start program", "", MB_OK );
-        //return "";
+        printf( "CreateProcess failed (%d).\n", GetLastError() );
+        return;
     }
 
     // Close handle to process
@@ -106,7 +106,7 @@ SCENARIO("Tests can run external programs", "[run_program]") {
         std::string path("./src/");
         std::string program(computePlatformSpecificProgramName("hello"));
 
-        std::string fileName("./src/hello.txt");
+        std::string fileName("hello.txt");
 
         WHEN("we look for the program") {
             std::cout << "Here we are: ";
