f6d86e3dd45b8b2bc82a32566f7bf613afe3acb9
Sylvie <sylvie@users.noreply.github.com>
2018-06-25 13:52:19 +0200

    SMB-30: Tentative de tests d'intégration avec AppVeyor (8)
    Intégration de l'exemple de programme parent appelant un enfant en redirigeant les flux.

--- test/integration/Makefile.mk
+++ test/integration/Makefile.mk
@@ -3,3 +3,8 @@ TESTS += test/integration/IntegrationTests
 
 test_integration_IntegrationTests_SOURCES = test/integration/HelloIntegrationTests.cpp ${catch_source_files}
 test_integration_IntegrationTests_CPPFLAGS = -I $(top_srcdir)/src -I $(top_srcdir)/test -Dtiny_sam_executable_extension=\"${tiny_sam_executable_extension}\"
+
+if WINDOWS
+test_integration_CreateParentProcess_SOURCES = test/integration/CreateParentProcess.cpp test/integration/strsafe.h
+test_integration_CreateChildProcess_SOURCES = test/integration/CreateChildProcess.cpp test/integration/strsafe.h
+endif
