7ef6b73cbc091fe6335bb238ff69721b25c8714b
Sylvie <sylvie@users.noreply.github.com>
2018-06-27 14:29:37 +0200

    SMB-30: Tentative de tests d'intégration avec une classe d'utilitaires (5)
    Déplacement des tests "CreateProcess" pour Windows dans un dossier "createprocess".

--- test/integration/Makefile.mk
+++ test/integration/Makefile.mk
@@ -5,10 +5,5 @@ test_integration_IntegrationTests_SOURCES = test/integration/HelloIntegrationTes
 test_integration_IntegrationTests_CPPFLAGS = -I $(top_srcdir)/src -I $(top_srcdir)/test -Dtiny_sam_executable_extension=\"${tiny_sam_executable_extension}\"
 test_integration_IntegrationTests_LDADD = $(top_builddir)/test/integration/utils/libTinySamIntegrationTestUtils.a
 
-if WINDOWS
-noinst_PROGRAMS += test/integration/CreateParentProcess test/integration/CreateChildProcess
-test_integration_CreateParentProcess_SOURCES = test/integration/CreateParentProcess.cpp test/integration/strsafe.h
-test_integration_CreateChildProcess_SOURCES = test/integration/CreateChildProcess.cpp test/integration/strsafe.h
-endif
-
 include test/integration/utils/Makefile.mk
+include test/integration/createprocess/Makefile.mk
